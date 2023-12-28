23ee6624e6b0863ef0f6e8e9725673808e8a2668
NekruzAsh <nekruzashrapov16@gmail.com>
2023-12-27 22:34:31 -0500

    Add .env file and server.js with API endpoints.

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
+
 export default function Home() {
   return (
     <body class="flex flex-col items-center justify-center w-screen min-h-screen text-gray-800 p-10">
